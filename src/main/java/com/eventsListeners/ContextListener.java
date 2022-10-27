a87026612b401116903045007b10aa0824e98437
Georgenjeru <njerugeorge57@gmail.com>
2022-10-27 14:50:42 +0300

    resource creation

--- src/main/java/com/eventsListeners/ContextListener.java
+++ src/main/java/com/eventsListeners/ContextListener.java
@@ -1,17 +1,20 @@
 package com.eventsListeners;
 
-import com.mysql.jdbc.Connection;
-import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
-import java.sql.DriverManager;
+import java.sql.Connection;
+
 
 @WebListener
 public class ContextListener implements ServletContextListener {
+
+    @Resource(lookup = "java:jboss/datasources/library")
+    DataSource dataSource;
 
     public void contextInitialized(ServletContextEvent ctxe){
         System.out.print("Library Application Starting up....initializing default attributes");
@@ -20,16 +23,10 @@ public class ContextListener implements ServletContextListener {
 
         try{
             System.out.println("Looking for Connection..");
-            /*BasicDataSource dataSource = new BasicDataSource();
-            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/admin");
-            dataSource.setUsername("root");
-            dataSource.setPassword("George@23");
             Connection connection = (Connection) dataSource.getConnection();
-            ctx.setAttribute("dbConnection",connection);*/
+            ctx.setAttribute("dbConnection",connection);
             System.out.println("Connection Found..");
 
-            Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/admin", "root", "George@23");
-            ctx.setAttribute("dbConnection", connection);
 
         }
         catch (Exception ex){
