f3cbf53a792a42d8a7d906fb1dec1baef973c068
Georgenjeru <njerugeorge57@gmail.com>
2022-11-13 16:06:11 +0300

    rest API

--- src/main/java/com/restapi/BookApi.java
+++ src/main/java/com/restapi/BookApi.java
@@ -1,4 +1,44 @@
 package com.restapi;
 
-public class BookApi {
+
+import com.bean.BookBeanI;
+import com.model.Book;
+import com.rest.BaseRestApi;
+import com.rest.ResponseWrapper;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/book")
+public class BookApi extends BaseRestApi {
+
+    @EJB
+    private BookBeanI bookBean;
+
+    @Path("/add")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response add(Book book) {
+        try {
+            bookBean.add(book);
+            return Response.status(Response.Status.OK).entity(new ResponseWrapper()).build();
+
+        } catch (Exception ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(new ResponseWrapper(false, ex.getMessage())).build();
+        }
+
+    }
+
+    @Path("/list")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response list() throws Exception {
+        return Response.status(Response.Status.OK).entity(bookBean.getList()).build();
+    }
+
+
 }
